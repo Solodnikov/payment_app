f9828880d7d1e2bebf0e40ad780bd5e90d7f6ef0
Alexander Solodnikov <solodnikov@yandex.ru>
2025-05-07 08:37:08 +0300

    bug fix

--- core/settings.py
+++ core/settings.py
@@ -9,18 +9,19 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from decouple import Config, RepositoryEnv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env_path = BASE_DIR / '.env'
+config = Config(repository=RepositoryEnv(env_path))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g3&q*+4jg^vdalpvfu_%e(e2k6y1exz2(r4!an*!il(4pazy_q'  # noqa
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
